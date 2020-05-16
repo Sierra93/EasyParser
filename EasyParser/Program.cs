00a150646ce150833ccf607a1f22b5ffcbfbfd87
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-05-17 02:35:31 +0300

    Добавьте файлы проекта.

---
+++ EasyParser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace EasyParser {
+    class Program {
+        static void Main(string[] args) {
+            string url = "https://www.btb.su/about/articles/yandex-region/";
+            WebClient wc = new WebClient();
+            string str = wc.DownloadString(url);
+            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(str);
+            var result = htmlDocument.DocumentNode.SelectNodes("//li").Select(el => el.InnerText);
+
+            Console.ReadKey();
+        }
+    }
+}
